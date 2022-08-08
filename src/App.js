62f61e0d69542199b0ec509197d935741d3e556d
fedeeman <j.fedor38@gmail.com>
2022-08-08 16:56:40 +0200

    props

--- src/App.js
+++ src/App.js
@@ -5,62 +5,76 @@ const welcome = {
   title: 'React'
 };
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
 
-function App() {
+
+const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ];
+
   return (
     <div>
       <h1>{welcome.greeting + ' ' + welcome.title}</h1>
       <Search></Search>
       <hr />
-      <List></List>
-
+      <List list={stories} novinka={stories} ran={'dsada'}></List>
     </div>
-
   );
 }
 
-function List() {
+const List = (props) => {
+  console.log(props);
   return (
     <ul>
-      {list.map(item => {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
+      {props.list.map(item =>
+        (
+          <Item item={item} key={item.objectID}></Item>
         )
-      })}
+      )}
     </ul>
   );
 }
 
-function Search() {
+const Item = (props) => {
+  return (
+    <li key={props.item.objectID}>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+};
+
+
+const Search = () => {
+
+  const handleChange = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  };
   return (
     <div>
       <label htmlFor="search">Search:</label>
-      <input id="search" type="text"></input>
+      <input id="search" type="text" onChange={handleChange}></input>
     </div>
   );
 }
