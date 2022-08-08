7f5c878dbf45c0b048eba4b9994f6234df5b35ec
fedeeman <j.fedor38@gmail.com>
2022-08-08 12:52:07 +0200

    making components

--- src/App.js
+++ src/App.js
@@ -28,27 +28,40 @@ function App() {
   return (
     <div>
       <h1>{welcome.greeting + ' ' + welcome.title}</h1>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text"></input>
-
+      <Search></Search>
       <hr />
+      <List></List>
 
-      <ul>
-        {list.map(item => {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          )
-        })}
-      </ul>
     </div>
 
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(item => {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        )
+      })}
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text"></input>
+    </div>
   );
 }
 
