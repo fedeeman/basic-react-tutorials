2cc98d12dfba47e84ae58f0f60699bfc31d5a963
fedeeman <j.fedor38@gmail.com>
2022-08-11 14:48:34 +0200

    fragments

--- src/App.js
+++ src/App.js
@@ -91,14 +91,14 @@ const Item = ({ item }) => {
 
 const Search = ({ search, onSearch }) => {
   return (
-    <div>
+    <>
       <label htmlFor="search">Search:</label>
       <input
         id="search"
         type="text"
         value={search}
         onChange={onSearch}></input>
-    </div>
+    </>
   );
 }
 
